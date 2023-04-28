f4e6c8eb288980c0fdbaa0a8305891acf1e4fd78
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-04-28 15:54:48 +0530

    Added WelcomePage to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import { BrowserRouter as Router, Route, } from "react-router-dom";
-import Header from './components/Header';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import WelcomePage from "./components/WelcomePage";
 
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
+      <Route path="/" exact component={WelcomePage} />
     </Router>
   );
 }
