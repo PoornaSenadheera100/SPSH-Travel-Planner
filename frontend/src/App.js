fe71d095ccc7719f0900315a5145539cb2b28fd3
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-04-29 08:28:41 +0530

    Added signup page to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import WelcomePage from "./components/WelcomePage";
+import SignupPage from "./components/SignupPage";
 
 function App() {
   return (
     <Router>
       <Header />
       <Route path="/" exact component={WelcomePage} />
+      <Route path="/signup" exact component={SignupPage} />
     </Router>
   );
 }
