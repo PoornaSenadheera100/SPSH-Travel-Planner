78e743d1e361e395bc989943943963dfa7f5fe53
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-04-29 14:10:44 +0530

    Added margins

--- frontend/src/components/AdminHomePage.js
+++ frontend/src/components/AdminHomePage.js
@@ -4,7 +4,10 @@ import CreateForm from "./CreateForm";
 export default function AdminHomePage() {
   const title = "test";
   return (
-    <div className="container row">
+    <div className="row">
+      <div style={{ width: "1px" }}>
+        <p style={{ color: "white" }}>Invisible</p>
+      </div>
       <div className="col-4">
         <button
           type="button"
@@ -33,16 +36,21 @@ export default function AdminHomePage() {
         </button>
       </div>
       <div className="col">
-        <Router>
-          <Route
-            path="/admin/addserviceprovider"
-            exact
-            render={(props) => (
-              <CreateForm {...props} title="Add Service Provider" />
-            )}
-          />
-          {/* <Route path="/signup" exact component={SignupPage} /> */}
-        </Router>
+        <div className="container">
+          <Router>
+            <Route
+              path="/admin/addserviceprovider"
+              exact
+              render={(props) => (
+                <CreateForm {...props} title="Add Service Provider" />
+              )}
+            />
+            {/* <Route path="/signup" exact component={SignupPage} /> */}
+          </Router>
+        </div>
+      </div>
+      <div style={{ width: "1px" }}>
+        <p style={{ color: "white" }}>Invisible</p>
       </div>
     </div>
   );
