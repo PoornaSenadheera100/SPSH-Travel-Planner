134c3135fc1f7a62e139f7a184fd3f5c70ba0ddd
Sathira-Wijeratne <swije27@gmail.com>
2023-05-01 13:31:32 +0530

    formatted code and fixed basic allignment

--- frontend/src/components/UserHome.js
+++ frontend/src/components/UserHome.js
@@ -1,30 +1,58 @@
-import { useState } from 'react';
-import UserServicesComponent from './UserServicesComponent';
+import { useState } from "react";
+import UserServicesComponent from "./UserServicesComponent";
 
-export default function UserHome(){
-    let [location, setLocation] = useState("");
-    const locations = ['Nuwara Eliya', 'Colombo', 'Kandy']; //get the locations from DB to here 
+export default function UserHome() {
+  let [location, setLocation] = useState("");
+  const locations = ["Nuwara Eliya", "Colombo", "Kandy"]; //get the locations from DB to here
 
-    return(
-        <div class="container">
-            <div class="button-row">
-                <button class="btn btn-dark" style={{marginBottom: '10px', width: '15%'}}>My Profile</button><br/>
-                <button class="btn btn-dark" style={{marginBottom: '10px', width: '15%'}}>My Requests</button><br/>
-            </div>
-            <div class="form-group">
-                <label>Enter Location</label><br/>
-                <input type="text" id="location" placeholder="Eg: Nuwara Eliya" required onChange={(e)=>{
-                    setLocation(e.target.value);
-                }}></input>
-
-                {/* Display Component based on location */}
-                {locations.map(city => {
-                    if (location === city) {
-                        return <UserServicesComponent city={city} />;
-                    }
-                }
-            )}
-            </div>
+  return (
+    <div class="container">
+      <div class="button-row">
+        <button
+          class="btn btn-dark"
+          style={{ marginBottom: "10px", width: "15%" }}
+        >
+          My Profile
+        </button>
+        <br />
+        <button
+          class="btn btn-dark"
+          style={{ marginBottom: "10px", width: "15%" }}
+        >
+          My Requests
+        </button>
+        <br />
+      </div>
+      <div
+        class="centered-form"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div class="form-group">
+          <label>Enter Location</label>
+          <br />
+          <input
+            type="text"
+            id="location"
+            placeholder="Eg: Nuwara Eliya"
+            required
+            onChange={(e) => {
+              setLocation(e.target.value);
+            }}
+          ></input>
         </div>
-    )
+      </div>
+      <div>
+        {/* Display Component based on location */}
+        {locations.map((city) => {
+          if (location.toLowerCase() === city.toLowerCase()) {
+            return <UserServicesComponent city={city} />;
+          }
+        })}
+      </div>
+    </div>
+  );
 }
