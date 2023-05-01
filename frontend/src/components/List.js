607081329cfb2e78abdc755439c4e61c9a8cdea7
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-05-01 12:13:48 +0530

    Removed column1name

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -24,90 +24,81 @@ export default function List(props) {
       </center>
       {list.length === 0 && (
         <h2>
+          <br />
           No Records
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
         </h2>
       )}
 
       {list.length !== 0 && (
-        <table className="table table-borderless">
-          <tr>
-            <th>
-              <center>{props.column1name}</center>
-            </th>
-            <th></th>
-            <th></th>
-            <th></th>
-          </tr>
+        <>
+          <br />
+          <table className="table table-borderless">
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
 
-          <tbody>
-            {list.map((item) => (
-              <tr>
-                <td>
-                  <center>{item.name}</center>
-                </td>
+            <tbody>
+              {list.map((item) => (
+                <tr>
+                  <td>
+                    <center>{item.name}</center>
+                  </td>
 
-                <td>
-                  <button
-                    className="btn btn-success btn-sm"
-                    onClick={() => {
-                      window.location.replace(`${props.viewURL}/${item.email}`);
-                    }}
-                  >
-                    View <i class="fa fa-pencil"></i>
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="btn btn-warning btn-sm"
-                    onClick={() => {
-                      window.location.replace(
-                        `${props.updateURL}/${item.email}`
-                      );
-                    }}
-                  >
-                    Update <i class="fa fa-pencil"></i>
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() => {
-                      var response = window.confirm(
-                        "Are you sure you want to delete this user?"
-                      );
-                      if (response) {
-                        axios
-                          .delete(`${props.deleteURL}/${item.email}`)
-                          .then(() => {
-                            alert(props.deleteMsg);
-                            window.location.replace(props.afterDeleteURL);
-                          })
-                          .catch((err) => {
-                            alert(err);
-                          });
-                      }
-                    }}
-                  >
-                    Delete <i class="fa fa-trash-o fa-lg"></i>
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                  <td>
+                    <button
+                      className="btn btn-success btn-sm"
+                      onClick={() => {
+                        window.location.replace(
+                          `${props.viewURL}/${item.email}`
+                        );
+                      }}
+                    >
+                      View <i class="fa fa-pencil"></i>
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-warning btn-sm"
+                      onClick={() => {
+                        window.location.replace(
+                          `${props.updateURL}/${item.email}`
+                        );
+                      }}
+                    >
+                      Update <i class="fa fa-pencil"></i>
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger btn-sm"
+                      onClick={() => {
+                        var response = window.confirm(
+                          "Are you sure you want to delete this user?"
+                        );
+                        if (response) {
+                          axios
+                            .delete(`${props.deleteURL}/${item.email}`)
+                            .then(() => {
+                              alert(props.deleteMsg);
+                              window.location.replace(props.afterDeleteURL);
+                            })
+                            .catch((err) => {
+                              alert(err);
+                            });
+                        }
+                      }}
+                    >
+                      Delete <i class="fa fa-trash-o fa-lg"></i>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       )}
     </>
   );
