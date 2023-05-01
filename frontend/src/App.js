b5be54f8c05f2d5046f5f567009b14d96de938b9
Sathira-Wijeratne <swije27@gmail.com>
2023-05-01 11:29:41 +0530

    added routerpath for userHome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Route, } from "react-router-dom";
 import Header from './components/Header';
+import UserHome from './components/UserHome';
 
 function App() {
   return (
     <Router>
       <Header/>
+      <Route path='/userHome' exact component={UserHome}/>
     </Router>
   );
 }
