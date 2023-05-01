0a37836e0f22ab29c7edc5e8aee23763e8905cae
SanuthiVihansa <subasofts@gmail.com>
2023-05-01 18:49:52 +0530

    Add new Tourist route

---
+++ backend/routes/tourist.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+let Tourist = require("../models/Tourist");
+
+
+// Add new Tourist
+router.route("/add").post((req, res)=>{
+    const name = req.body.name;
+    const nic = req.body.nic;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const password = req.body.password;
+
+// Creating a new Tourist object with extracted details
+    const newTourist = new Tourist({
+        name,
+        nic,
+        email,
+        phone,
+        password
+    })
+
+    // Saving the new Tourist object in the database
+    newTourist.save().then(()=>{
+        res.json("Tourist Added.");
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+
+module.exports = router;
