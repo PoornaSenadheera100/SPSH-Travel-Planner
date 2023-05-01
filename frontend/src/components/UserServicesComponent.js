69af764ec783b8e124d0ebc942d56052e0389c51
Sathira-Wijeratne <swije27@gmail.com>
2023-05-01 17:40:04 +0530

    made form appear on button click

--- frontend/src/components/UserServicesComponent.js
+++ frontend/src/components/UserServicesComponent.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import UserServiceBookingForm from "./UserServiceBookingForm";
 
 export default function UserServicesComponent(props) {
   let { city } = props;
@@ -11,40 +12,54 @@ export default function UserServicesComponent(props) {
     "Pony ride in racecourse",
     "Horseback riding on the beach",
   ]);
-  let [serviceImages, setServiceImage] = useState("");
+
+  let [showServiceComponents, setShowServiceComponents] = useState(true);
+
+  const handleServiceClick = (index) => {
+    setShowServiceComponents(false);
+    document.getElementById("myForm").style.display = "block";
+  };
 
   return (
     <div>
       <ul>
-        {serviceNames.map((name, index) => (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <a href="#" style={{ textDecoration: "none" }}>
-              <div
-                key={index}
-                style={{
-                  border: "1px solid black",
-                  margin: "10px",
-                  backgroundColor: "#000080",
-                  borderRadius: "5px",
-                  padding: "20px",
-                  minWidth: "275px",
-                }}
+        {showServiceComponents &&
+          serviceNames.map((name, index) => (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href="#"
+                style={{ textDecoration: "none" }}
+                onClick={() => handleServiceClick(index)}
               >
-                <h3 style={{ margin: "0", color: "#fff", fontSize: "24px" }}>
-                  {name}
-                </h3>
-                <p style={{ color: "#fff" }}>{serviceDescriptions[index]}</p>
-              </div>
-            </a>
-          </div>
-        ))}
+                <div
+                  key={index}
+                  style={{
+                    border: "1px solid black",
+                    margin: "10px",
+                    backgroundColor: "#000080",
+                    borderRadius: "5px",
+                    padding: "20px",
+                    minWidth: "275px",
+                  }}
+                >
+                  <h3 style={{ margin: "0", color: "#fff", fontSize: "24px" }}>
+                    {name}
+                  </h3>
+                  <p style={{ color: "#fff" }}>{serviceDescriptions[index]}</p>
+                </div>
+              </a>
+            </div>
+          ))}
       </ul>
+      <div id="myForm" style={{ display: "none" }}>
+        <UserServiceBookingForm serviceName={serviceNames} />
+      </div>
     </div>
   );
 }
