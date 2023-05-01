0c9d0e2f51c3b0dde891894e4950c600bf4f2122
SanuthiVihansa <subasofts@gmail.com>
2023-05-01 19:06:41 +0530

    Get one tourists information by providing the email

--- backend/routes/tourist.js
+++ backend/routes/tourist.js
@@ -36,5 +36,15 @@ route.route("/").get((req,res)=>{
     })
 })
 
+//Get one tourists information by providing the email
+router.route("/get/email/:email").get(async(req, res)=>{
+    let email = req.params.email;
+    await Seller.find({"email": `${email}`}).then((seller)=>{
+        res.json(seller);
+    }).catch((err)=>{
+        console.log(err.message);
+        res.status(500).send({status: "Error with get the seller", error: err.message});
+    })
+})
 
 module.exports = router;
