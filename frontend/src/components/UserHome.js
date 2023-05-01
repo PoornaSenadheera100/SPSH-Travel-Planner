c4756c30f0c6455fc6e8ba2f171925b16422e707
Sathira-Wijeratne <swije27@gmail.com>
2023-05-01 12:48:26 +0530

    map function display service according to location

--- frontend/src/components/UserHome.js
+++ frontend/src/components/UserHome.js
@@ -1,6 +1,10 @@
-import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import UserServicesComponent from './UserServicesComponent';
 
 export default function UserHome(){
+    let [location, setLocation] = useState("");
+    const locations = ['Nuwara Eliya', 'Colombo', 'Kandy']; //get the locations from DB to here 
+
     return(
         <div class="container">
             <div class="button-row">
@@ -9,7 +13,17 @@ export default function UserHome(){
             </div>
             <div class="form-group">
                 <label>Enter Location</label><br/>
-                <input type="text" id="location" placeholder="Eg: Nuwara Eliya"></input>
+                <input type="text" id="location" placeholder="Eg: Nuwara Eliya" required onChange={(e)=>{
+                    setLocation(e.target.value);
+                }}></input>
+
+                {/* Display Component based on location */}
+                {locations.map(city => {
+                    if (location === city) {
+                        return <UserServicesComponent city={city} />;
+                    }
+                }
+            )}
             </div>
         </div>
     )
