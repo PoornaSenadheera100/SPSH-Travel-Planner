e72afa8c2f70fc6f0b6b3c835dcd6e8057bf83ba
HilarinaChristy <it21127946@my.sliit.lk>
2023-05-02 09:07:56 +0530

    Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,27 @@
-export default function Headers(){
-    return(
-        <div>
-            <h1>This is the sample header</h1>
+import React from "react";
+import logo from "../images/spsh_logo.png";
+
+export default function Header() {
+  return (
+    <div>
+      <nav className="navbar fixed-top bg-dark" style={{ color: "white" }}>
+        <div className="row">
+          <div className="col-3">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <img src={logo} width="40%" alt="Logo"></img>
+          </div>
+          <div className="col" style={{ fontSize: "80px" }}>
+            <b>SPSH Travel Planner</b>
+          </div>
         </div>
-    )
+      </nav>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+    </div>
+  );
 }
