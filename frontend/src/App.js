c98aa6d7412912fc79b4cbbe8be4b4708ff437da
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-05-03 20:10:37 +0530

    Added ServiceProviderHomePage to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import WelcomePage from "./components/WelcomePage";
 import SignupPage from "./components/SignupPage";
 import AdminHomePage from "./components/AdminHomePage";
+import ServiceProviderHomePage from "./components/ServiceProviderHomePage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" exact component={WelcomePage} />
       <Route path="/signup" exact component={SignupPage} />
       <Route path="/admin/" component={AdminHomePage} />
+      <Route path="/serviceprovider/" component={ServiceProviderHomePage} />
     </Router>
   );
 }
