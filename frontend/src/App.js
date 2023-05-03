37c49d1ae6394eaf0d9d04c9055fdb978c472dcd
HilarinaChristy <it21127946@my.sliit.lk>
2023-05-03 21:44:50 +0530

    Modified Route paths.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,15 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import AllServices from "./components/AllServices";
 import AddService from "./components/AddService";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Route
-        path="/sellerhome/service/add/"
-        exact
-        component={AddService}
-      ></Route>
+      <Route path="/service/add/" exact component={AddService}></Route>
+      <Route path="/service" exact component={AllServices}></Route>
       <Footer />
     </Router>
   );
