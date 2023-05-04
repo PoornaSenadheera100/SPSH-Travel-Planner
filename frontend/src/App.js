a1bb6555bb2e6f33b45ec556bc318d37cba84e4d
SanuthiVihansa <subasofts@gmail.com>
2023-05-04 15:30:54 +0530

    formatText

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import { BrowserRouter as Router, Route, } from "react-router-dom";
-import Header from './components/Header';
-import TouristProfile from './components/TouristProfile';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import TouristProfile from "./components/TouristProfile";
 
 function App() {
   return (
     <Router>
-      
-      <Route path = '/' exact component = {Header}/>
-      <Route path = '/tourist/view/userprofile' exact component = {TouristProfile}/>
-      
+      <Route path="/" exact component={Header} />
+      <Route
+        path="/tourist/view/userprofile"
+        exact
+        component={TouristProfile}
+      />
     </Router>
   );
 }
