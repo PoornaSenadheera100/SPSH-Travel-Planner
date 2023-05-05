47b2ea8b6cb7521e9ec14616f2e6c1a754bf5b6a
Sathira-Wijeratne <swije27@gmail.com>
2023-05-05 12:40:21 +0530

    added new routepaths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
-import { BrowserRouter as Router, Route, } from "react-router-dom";
-import Header from './components/Header';
-import UserHome from './components/UserHome';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import UserHome from "./components/UserHome";
+import UserRequests from "./components/UserRequests";
+import UserViewService from "./components/UserViewService";
+import RequestDetails from "./components/RequestDetails";
 
 function App() {
   return (
     <Router>
-      <Header/>
-      <Route path='/userHome' exact component={UserHome}/>
+      <Header />
+      {/* <Route path="/userHome" exact component={UserHome} /> */}
+      <Route path="/" exact component={UserHome} />
+      {/* <Route path="/userHome/requests/:id" exact component={UserRequests} /> */}
+      <Route path="/userHome/requests/" exact component={UserRequests} />
+      <Route
+        path="/userHome/requests/view/"
+        exact
+        component={UserViewService}
+      />
+      {/* <Route path="/userHome/requests/view/" exact component={RequestDetails} /> */}
     </Router>
   );
 }
