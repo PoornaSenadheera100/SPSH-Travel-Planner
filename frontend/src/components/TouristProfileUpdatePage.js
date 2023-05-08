e9744672eb06235ed370108713c3ae2e121e31ab
SanuthiVihansa <subasofts@gmail.com>
2023-05-08 21:13:49 +0530

    commit changes

--- frontend/src/components/TouristProfileUpdatePage.js
+++ frontend/src/components/TouristProfileUpdatePage.js
@@ -1,33 +1,45 @@
+import { useState } from "react";
 import CreateForm from "./CreateForm";
 
 export default function TouristProfileUpdatePage(){
 
-     //Function to update the data
-  function updateProfile(e) {
-    e.preventDefault();
+  const pwdChangeWarning = <><br></br><h4>Leave the below fields blank if you do not want to change the password</h4></>
 
-    const newTourist = {
-      name,
-      address,
-      nic,
-      phone,
-      email,
-      password,
-    };
-    axios
-      .put(`http://localhost:8070/tourist/update/${email}`, newTourist)
-      .then(() => {
-        alert("Profile updated");
-        window.location.replace("http://localhost:3000/tourist/");
-      })
-      .catch((err) => {
-        alert("Update failure occured ! ");
-      });
-  }
+  // const [name,setName]=useState("");
+  // const [email,setEmail]=useState(""); //This i should not allow to update neda
+  // const [nic,setNic]=useState("");
+  // const [phone,setPhone]=useState("");
+  // const [newpassword,setNewPassword]=useState("");
+  // const[password,password]=useState("");
+
+
+
+//Function to update the data
+  // function updateProfile(e) {
+  //   e.preventDefault();
+
+  //   const newTourist = {
+  //     name,
+  //     address,
+  //     nic,
+  //     phone,
+  //     email,
+  //     password,
+  //   };
+  //   axios
+  //     .put(`http://localhost:8070/tourist/update/${email}`, newTourist)
+  //     .then(() => {
+  //       alert("Profile updated");
+  //       window.location.replace("http://localhost:3000/tourist/");
+  //     })
+  //     .catch((err) => {
+  //       alert("Update failure occured ! ");
+  //     });
+  
 
     return(<div className="container">
 
-        <CreateForm title='Update profile' getURL='http://localhost:8070/tourist/get/email'/>
+        <CreateForm title='Update tourist profile' getURL='http://localhost:8070/tourist/get/email' pwdChangeWarning={pwdChangeWarning}/>
 
     </div>)
 
