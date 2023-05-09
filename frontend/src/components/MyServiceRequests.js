7bd242731c9b19a0d7789427519e01ee97446aed
HilarinaChristy <it21127946@my.sliit.lk>
2023-05-09 19:43:09 +0530

    Accepted service request page.

--- frontend/src/components/MyServiceRequests.js
+++ frontend/src/components/MyServiceRequests.js
@@ -1,105 +1,86 @@
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
-export default function MyServiceRequest() {
+export default function MyServiceRequests() {
   if (sessionStorage.getItem("sTravPlaVresVorp") === null) {
     window.location.replace("/");
   }
 
-  const touristEmail = sessionStorage.getItem("touristEmail");
-  const serviceProvEmail = sessionStorage.getItem("sTravPlaTsirout");
-  const [services, setServices] = useState([]);
+  const [serviceRequests, setServiceRequests] = useState([]);
+  const Service_ProviderId = sessionStorage.getItem("serviceProviderEmail");
 
   useEffect(() => {
-    axios
-      .get(
-        `http://localhost:8070/servicerequest/get/email/${touristEmail}/${serviceProvEmail}`
-      )
-      .then((res) => {
-        setServices(res.data);
-        console.log(res.data);
-      });
+    const getServiceRequests = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8070/servicerequest/myrequest/${Service_ProviderId}`
+        );
+        const acceptedServiceRequests = response.data.filter(
+          (serviceRequest) => serviceRequest.status === "Accepted"
+        );
+        //To check if data is fetched properly.
+        console.log(response.data);
+        setServiceRequests(response.data);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+
+    getServiceRequests();
   }, []);
 
   return (
     <div>
-      <a href="/service">
-        <button
-          className="btn btn-dark"
-          style={{
-            display: "inline-block",
-            textAlign: "left",
-            marginLeft: "10px",
-          }}
-        >
-          Back
-        </button>
-      </a>
-      <div className="container">
-        <center>
-          <h1>Bookings Made</h1>
-        </center>
-        {services.length === 0 && <h1>No Bookings</h1>}
-        {services.length !== 0 && (
-          <table className="table table-borderless">
-            <tr>
-              <th>
-                <center>Tourist Name</center>
-              </th>
-              <th>
-                <center>Service Name</center>
-              </th>
-              <th>
-                <center>Contact No</center>
-              </th>
-              <th>
-                <center>No. of People</center>
-              </th>
-              <th></th>
-            </tr>
-
+      <button
+        class="btn btn-dark"
+        style={{
+          maxWidth: "100%",
+          height: "40px",
+          width: "120px",
+          margin: "40px",
+          whiteSpace: "nowrap",
+        }}
+        onClick={() => {
+          //history.push moves from the current page.
+          //history.push(`/update/${student._id}`);
+          //window.location also redirects to another page.(delete page with the ID)
+          window.location.replace(`http://localhost:3000/serviceprovider/`);
+        }}
+      >
+        Back
+      </button>
+      <center>
+        <h2>Service Requests Accepted</h2>
+      </center>
+      <ul>
+        <table className="table table-borderless">
+          <tr>
+            <th>
+              <center>Tourist Name</center>
+            </th>
+            <th>
+              <center>Tourist Contact No</center>
+            </th>
+            <th>
+              <center>No.of People</center>
+            </th>
+            <th></th>
+          </tr>
+          {serviceRequests.map((servicerequest) => (
             <tbody>
-              {services.map((service) => (
-                <tr>
-                  <td>
-                    <center>{service.orderRef}</center>
-                  </td>
-                  <td>
-                    <center>{service.paymentmethod}</center>
-                  </td>
-                  <td>
-                    <center>{service.status}</center>
-                  </td>
-                  <td
-                    style={{
-                      color:
-                        order.appStatus === "Rejected"
-                          ? "red"
-                          : order.appStatus === "Approved"
-                          ? "green"
-                          : "orange",
-                    }}
-                  >
-                    <center>{service.appStatus}</center>
-                  </td>
-                  <td>
-                    <button
-                      className="btn btn-warning btn-sm"
-                      onClick={() => {
-                        window.location.replace(
-                          `http://localhost:3000/buyerhome/myorders/${order.orderRef}`
-                        );
-                      }}
-                    >
-                      View <i class="fa fa-pencil"></i>
-                    </button>
-                  </td>
-                </tr>
-              ))}
+              <td>
+                <center>{servicerequest.name} </center>
+              </td>
+              <td>
+                <center>{servicerequest.contactNo}</center>
+              </td>
+              <td>
+                <center>{servicerequest.quantity}</center>
+              </td>
             </tbody>
-          </table>
-        )}
-      </div>
+          ))}
+        </table>
+      </ul>
     </div>
   );
 }
