32dc1ca156f2192ec2097de7788800819ab02913
HilarinaChristy <it21127946@my.sliit.lk>
2023-05-10 23:41:47 +0530

    Implemented "View" My requests.

--- frontend/src/components/UserRequests.js
+++ frontend/src/components/UserRequests.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import { Link, useHistory } from "react-router-dom";
 import UserViewService from "./UserViewService";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -8,46 +9,68 @@ export default function UserRequests() {
     window.location.replace("/");
   }
 
+  const touristEmail = sessionStorage.getItem("touristEmail");
+  const [requests, setRequests] = useState([]);
+
   let [status, setStatus] = useState(["Approved", "Rejected", "Pending"]);
+
+  useEffect(() => {
+    function getRequest() {
+      axios
+        .get(`http://localhost:8070/servicerequest/get/email/${touristEmail}`)
+        .then((res) => {
+          console.log(res.data);
+          setRequests(res.data);
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    }
+    getRequest();
+  }, []);
 
   return (
     <div className="container" style={{ marginTop: "20px" }}>
-      <h1 style={{ paddingBottom: "20px" }}>My Requests</h1>
-      {status.map((status, index) => (
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            border: "1px solid black",
-            margin: "10px",
-            borderRadius: "5px",
-            padding: "20px",
-          }}
-        >
-          <h3>Request {index + 1}</h3>
-          <h4
-            style={{
-              color:
-                status === "Rejected"
-                  ? "red"
-                  : status === "Approved"
-                  ? "green"
-                  : "black",
-            }}
-          >
-            Status: {status}
-          </h4>
-          <a
-            href="/tourist/requests/view"
-            className="btn btn-secondary"
-            style={{ float: "right" }}
-          >
-            View
-          </a>
-        </div>
-      ))}
-
+      <div>
+        {requests.map((request) => (
+          <div>
+            <h1 style={{ paddingBottom: "20px" }}>My Requests</h1>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                border: "1px solid black",
+                margin: "10px",
+                borderRadius: "5px",
+                padding: "20px",
+              }}
+            >
+              <h3>Request</h3>
+              {request.bookingId}
+              <h4
+                style={{
+                  color:
+                    request.status === "Rejected"
+                      ? "red"
+                      : request.status === "Approved"
+                      ? "green"
+                      : "black",
+                }}
+              >
+                Status: {request.status}
+              </h4>
+              <a
+                href="/tourist/requests/view"
+                className="btn btn-secondary"
+                style={{ float: "right" }}
+              >
+                View
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
       {/* pass status as prop to show cancel button according to that */}
       {/* <Router>
         <Route
