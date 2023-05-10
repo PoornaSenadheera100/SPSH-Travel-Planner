05f2c3004f608e04e0ad209f4fb470de121fb9f0
Sathira-Wijeratne <swije27@gmail.com>
2023-05-10 21:24:01 +0530

    implemented loading component

---
+++ frontend/src/components/LoadingSpinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Spinner } from "reactstrap";
+// import styles from "../styles/LoadingSpinner.module.css";
+const LoadingSpinner = () => {
+  return (
+    <div>
+      <Spinner color="primary" />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
